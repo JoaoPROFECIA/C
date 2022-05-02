9e09eefdf82ed51b290967e52590acafbfb72b52
Joao Victor <victorjv3288@gmail.com>
2022-05-02 20:00:11 -0300

    Comandos básicos de C

---
+++ IDE/0014 - Escape Sequence/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    printf("\tEu sou o Code::Block\n");
+/*
+
+\' - Aspa simples
+\" - Aspa dupla
+\? - Interrogação
+\\ - backslash
+\0 - Caractere null (fim de uma String)
+\a - Emissão de som
+\b - backspace
+\f - Nova página (form feed)
+\n - Nova página (line feed)
+\r - Retorno do cursor (carriage return)
+\t - Tabulação (tab)
+\v - Tabulação Vertical
+
+*/
+    return 0;
+}
