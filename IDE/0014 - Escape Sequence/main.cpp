f40cbbbacf8402a46214de300342784b0c80e934
Joao Victor <victorjv3288@gmail.com>
2022-10-21 10:48:40 -0300

    Dados básicos de C

---
+++ IDE/0014 - Escape Sequence/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    cout << "Hello World!\r" << endl;
+    cout << "Hello World!\r";
+    return 0;
+}
---
+++ curso_C_para _Facul/aula_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h> // inclui a biblioteca padrão de entrada e saída
+#include <stdlib.h> // para usar o system("pause")
+
+int main(){
+
+    printf("Hello, World!\n\n");
+    
+    system("pause"); // para o programa não fechar automaticamente
+
+}
+
+void main1() {
+    printf("Hello, World 2!\n\n");
+}
